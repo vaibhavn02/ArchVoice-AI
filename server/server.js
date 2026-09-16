7ed787a716df5ada39d20aba350ac4ab067804a1
vaibhavn02 <vaibhavn02@gmail.com>
2026-09-16 13:57:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -44,8 +44,8 @@ app.get("/api/issues",(req,res)=>{
     });
 });
 
-app.listen(5000,()=>{
-    console.log(
-        "Server running on port 5000"
-    );
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
